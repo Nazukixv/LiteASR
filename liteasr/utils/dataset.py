a24e3258ec97592f44c4443c638c892b9c318775
Juncheng Cao <soukunsei@gmail.com>
2021-12-08 17:37:28 +0800

    :art: Change contents generated by `AudioFileDataset`

--- liteasr/utils/dataset.py
+++ liteasr/utils/dataset.py
@@ -17,13 +17,15 @@ class AudioFileDataset(Dataset):
     def __getitem__(self, index):
         """overload [] operator"""
         audios: List[Audio] = self.data[index]
-        xs, ys = [], []
+        xs, ys, xlens, ylens = [], [], [], []
         for audio in audios:
             xs.append(audio.x)
             ys.append(audio.y)
+            xlens.append(int(len(audio.x)))
+            ylens.append(int(len(audio.y)))
         padded_xs = pad(xs, 0)
-        padded_ys = pad(ys, -1)
-        return padded_xs, padded_ys
+        padded_ys = pad(ys, 0)
+        return padded_xs, padded_ys, xlens, ylens
 
     def __len__(self):
         """overload len() method"""
