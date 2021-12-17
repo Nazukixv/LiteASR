39dc0f4e4b653a2c5acd41612cefcea2c8e0b5cf
Juncheng Cao <soukunsei@gmail.com>
2021-12-18 03:28:10 +0800

    :zap: Improve mask generation implementation :zap: Reimplement the `triangle_mask` function and introduce `stage` parameter :bulb: Add usage examples in the docstring of each mask generation function

--- liteasr/utils/mask.py
+++ liteasr/utils/mask.py
@@ -1,6 +1,6 @@
 """Functions of mask generation."""
 
-from typing import List, Optional
+from typing import List
 
 import torch
 from torch import Tensor
@@ -9,27 +9,84 @@ from torch import Tensor
 def padding_mask(size: List[int]) -> Tensor:
     """Returns mask which masks the padding part of tensor.
 
-    :param size: [description]
+    :param size: sequence of unmasking lengths
     :type size: List[int]
+    :return: mask
+    :rtype: Tensor
+
+    :Example (ignoring zeros):
+
+    >>> padding_mask([5, 3, 1])
+    [[ ,  ,  ,  ,  ],
+     [ ,  ,  , 1, 1],
+     [ , 1, 1, 1, 1]]
+
     """
 
-    batch = len(size)
-    t_max = max(size)
-    base = torch.arange(0, t_max).unsqueeze(0).expand(batch, t_max)
-    lens = torch.tensor(size).unsqueeze(1).expand(batch, t_max)
+    base = torch.arange(max(size)).unsqueeze(0)
+    lens = torch.tensor(size).unsqueeze(1)
     mask = base >= lens
     return mask
 
 
 def triangle_mask(
-    width: int,
-    height: Optional[int] = None,
+    row: int,
+    col: int = 0,
+    stage: int = 1,
     diagonal: int = 1,
 ) -> Tensor:
-    """Returns triangle mask."""
+    """Returns triangle mask.
 
-    if height is None:
-        base = torch.ones(width, width).bool()
-    else:
-        base = torch.ones(width, height).bool()
-    return torch.triu(base, diagonal=diagonal)
+    :param int row: number of rows of mask
+    :param int col: number of columns of mask
+    :param int stage: width of each `stage` of triangle mask
+    :param int diagonal: diagonal position
+    :return: mask
+    :rtype: Tensor
+
+    :Example (ignoring zeros):
+
+    >>> triangle_mask(row=5)
+    [[ , 1, 1, 1, 1],
+     [ ,  , 1, 1, 1],
+     [ ,  ,  , 1, 1],
+     [ ,  ,  ,  , 1],
+     [ ,  ,  ,  ,  ]]
+
+    >>> triangle_mask(row=3, col=5)
+    [[ , 1, 1, 1, 1],
+     [ ,  , 1, 1, 1],
+     [ ,  ,  , 1, 1]]
+
+    >>> triangle_mask(row=3, col=5, diagonal=2)
+    [[ ,  , 1, 1, 1],
+     [ ,  ,  , 1, 1],
+     [ ,  ,  ,  , 1]]
+
+    >>> triangle_mask(row=8, stage=2)
+    [[ ,  , 1, 1, 1, 1, 1, 1],
+     [ ,  , 1, 1, 1, 1, 1, 1],
+     [ ,  ,  ,  , 1, 1, 1, 1],
+     [ ,  ,  ,  , 1, 1, 1, 1],
+     [ ,  ,  ,  ,  ,  , 1, 1],
+     [ ,  ,  ,  ,  ,  , 1, 1],
+     [ ,  ,  ,  ,  ,  ,  ,  ],
+     [ ,  ,  ,  ,  ,  ,  ,  ]]
+
+    >>> triangle_mask(row=8, stage=2, diagonal=2)
+    [[ ,  ,  ,  , 1, 1, 1, 1],
+     [ ,  ,  ,  , 1, 1, 1, 1],
+     [ ,  ,  ,  ,  ,  , 1, 1],
+     [ ,  ,  ,  ,  ,  , 1, 1],
+     [ ,  ,  ,  ,  ,  ,  ,  ],
+     [ ,  ,  ,  ,  ,  ,  ,  ],
+     [ ,  ,  ,  ,  ,  ,  ,  ],
+     [ ,  ,  ,  ,  ,  ,  ,  ]]
+
+    """
+
+    col = row if col == 0 else col
+    row_idx = torch.arange(row).unsqueeze(1)
+    col_idx = torch.arange(col).unsqueeze(0)
+    mask = (col_idx // stage) > ((row_idx // stage) + (diagonal - 1))
+    return mask
