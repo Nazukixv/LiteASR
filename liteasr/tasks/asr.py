84e59ef3fc894d3a0aa4448086a9befdbc830403
Juncheng Cao <soukunsei@gmail.com>
2022-02-23 16:15:22 +0800

    :recycle: Move batchify code into Dataset initialization

--- liteasr/tasks/asr.py
+++ liteasr/tasks/asr.py
@@ -8,13 +8,11 @@ from omegaconf import MISSING
 
 from liteasr.config import DatasetConfig
 from liteasr.config import LiteasrDataclass
-from liteasr.dataclass.audio_data import Audio
-from liteasr.dataclass.sheet import AudioSheet
-from liteasr.dataclass.sheet import TextSheet
 from liteasr.dataclass.vocab import Vocab
 from liteasr.models import LiteasrModel
 from liteasr.tasks import LiteasrTask
 from liteasr.tasks import register_task
+from liteasr.utils.dataset import AudioFileDataset
 
 logger = logging.getLogger(__name__)
 
@@ -53,42 +51,27 @@ class ASRTask(LiteasrTask):
                 os.path.dirname(self.train.scp)
             )
         )
-        train_data = self._load_dataset(self.train)
-        self.feat_dim = train_data[0].shape[-1]
+        self.train_set = AudioFileDataset(
+            scp=self.train.scp,
+            segments=self.train.segments,
+            text=self.train.text,
+            vocab=self.vocab,
+            cfg=cfg,
+        )
+        self.feat_dim = self.train_set.feat_dim
 
         logger.info(
             "loading validation data from {}".format(
                 os.path.dirname(self.valid.scp)
             )
         )
-        valid_data = self._load_dataset(self.valid)
-
-        # TODO: batchify
-        train_data = sorted(
-            train_data, key=lambda audio: audio.shape[0], reverse=True
+        self.valid_set = AudioFileDataset(
+            scp=self.valid.scp,
+            segments=self.valid.segments,
+            text=self.valid.text,
+            vocab=self.vocab,
+            cfg=cfg,
         )
-
-        while len(self._train_data) * cfg.batch_size < len(train_data):
-            self._train_data.append(
-                train_data[len(self._train_data)
-                           * cfg.batch_size:(len(self._train_data) + 1)
-                           * cfg.batch_size]
-            )
-
-        valid_data = sorted(
-            valid_data, key=lambda audio: audio.shape[0], reverse=True
-        )
-
-        while len(self._valid_data) * cfg.batch_size < len(valid_data):
-            self._valid_data.append(
-                valid_data[len(self._valid_data)
-                           * cfg.batch_size:(len(self._valid_data) + 1)
-                           * cfg.batch_size]
-            )
-
-        from liteasr.utils.dataset import AudioFileDataset
-        self.train_set = AudioFileDataset(self._train_data)
-        self.valid_set = AudioFileDataset(self._valid_data)
 
     def inference(self, x, model: LiteasrModel):
         tokenids = model.inference(x)
@@ -98,17 +81,3 @@ class ASRTask(LiteasrTask):
     def save_model(self, model_name: str, model: LiteasrModel):
         model_path = os.sep.join((self.save_dir, model_name))
         model.save(model_path)
-
-    def _load_dataset(self, cfg: DataConfig):
-        data = []
-        audio_sheet = AudioSheet(scp=cfg.scp, segments=cfg.segments)
-        text_sheet = TextSheet(text=cfg.text, vocab=self.vocab)
-        for audio_info, text_info in zip(audio_sheet, text_sheet):
-            uttid, fd, start, end, shape = audio_info
-            uttid_t, tokenids = text_info
-            assert uttid == uttid_t
-            data.append(Audio(uttid, fd, start, end, shape, tokenids=tokenids))
-            if len(data) % 10000 == 0:
-                logger.info("number of loaded data: {}".format(len(data)))
-        logger.info("number of loaded data: {}".format(len(data)))
-        return data
--- liteasr/utils/dataset.py
+++ liteasr/utils/dataset.py
@@ -1,27 +1,65 @@
 """Lite-ASR dataset implementation for training."""
 
+import logging
 from typing import List
 
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset
 
+from liteasr.config import DatasetConfig
 from liteasr.dataclass.audio_data import Audio
+from liteasr.dataclass.sheet import AudioSheet
+from liteasr.dataclass.sheet import TextSheet
+
+logger = logging.getLogger(__name__)
 
 
 class AudioFileDataset(Dataset):
 
-    def __init__(self, data):
-        super().__init__()
-        self.data = data
+    def __init__(
+        self,
+        scp: str,
+        segments: str,
+        text: str,
+        vocab,
+        cfg: DatasetConfig,
+    ):
+        self.data = []
+
+        _as = AudioSheet(scp=scp, segments=segments)
+        _ts = TextSheet(text=text, vocab=vocab)
+        for audio_info, text_info in zip(_as, _ts):
+            uttid, fd, start, end, shape = audio_info
+            uttid_t, tokenids = text_info
+            assert uttid_t == uttid
+            self.data.append(
+                Audio(uttid, fd, start, end, shape, tokenids=tokenids)
+            )
+            if len(self.data) % 10000 == 0:
+                logger.info("number of loaded data: {}".format(len(self.data)))
+        logger.info("number of loaded data: {}".format(len(self.data)))
+
+        self.batch_data = self.batchify(self.data, cfg)
+        self.feat_dim = self.data[0].shape[-1]
+
+    def batchify(self, samples, cfg):
+        samples = sorted(samples, key=lambda a: a.shape[0], reverse=True)
+        batch_data = []
+        while len(batch_data) * cfg.batch_size < len(samples):
+            batch_data.append(
+                samples[len(batch_data) * cfg.batch_size:(len(batch_data) + 1)
+                        * cfg.batch_size]
+            )
+        return batch_data
 
     def __getitem__(self, index):
         """overload [] operator"""
-        audios: List[Audio] = self.data[index]
-        xs, ys, xlens, ylens = [], [], [], []
+        audios: List[Audio] = self.batch_data[index]
+        xs, xlens, ys, ylens = [], [], [], []
         for audio in audios:
             xs.append(audio.x)
-            ys.append(audio.y)
             xlens.append(audio.xlen)
+            ys.append(audio.y)
             ylens.append(audio.ylen)
         padded_xs = pad_sequence(xs, batch_first=True, padding_value=0)
         padded_ys = pad_sequence(ys, batch_first=True, padding_value=-1)
@@ -29,4 +67,4 @@ class AudioFileDataset(Dataset):
 
     def __len__(self):
         """overload len() method"""
-        return len(self.data)
+        return len(self.batch_data)
