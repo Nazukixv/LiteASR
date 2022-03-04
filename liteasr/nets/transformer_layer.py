459476fdf33593906cd78ecd69764491cb068932
Juncheng Cao <soukunsei@gmail.com>
2022-03-04 15:59:52 +0800

    :spider_web: Implement relative encoder layer

--- liteasr/nets/transformer_layer.py
+++ liteasr/nets/transformer_layer.py
@@ -82,3 +82,59 @@ class EncoderLayer(nn.Module):
             x = torch.cat([cache, x], dim=1)
 
         return x
+
+
+class RelativeEncoderLayer(EncoderLayer):
+
+    def __init__(
+        self,
+        size,
+        self_attn,
+        feed_forward,
+        dropout_rate,
+        normalize_before=True,
+        concat_after=False,
+    ):
+        super().__init__(
+            size,
+            self_attn,
+            feed_forward,
+            dropout_rate,
+            normalize_before,
+            concat_after,
+        )
+
+    def forward(
+        self,
+        x_pos_emb,
+        mask: Optional[torch.Tensor] = None,
+        cache: Optional[torch.Tensor] = None,
+    ):
+        x, pos_emb = x_pos_emb[0], x_pos_emb[1]
+
+        # self attention + ResNet
+        residual = x
+        x = self.self_attn_norm(x) if self.normalize_before else x
+
+        if cache is None:
+            x_q = x
+        else:
+            assert cache.shape == (x.shape[0], x.shape[1] - 1, self.size)
+            x_q = x[:, -1:, :]
+            residual = residual[:, -1:, :]
+            mask = mask[:, -1:, :] if mask is not None else mask
+
+        x = self.dropout(self.self_attn(x_q, x, x, pos_emb, mask)) + residual
+        x = self.self_attn_norm(x) if not self.normalize_before else x
+
+        # feed-forward + ResNet
+        residual = x
+        x = self.feed_forward_norm(x) if self.normalize_before else x
+        x = self.dropout(self.feed_forward(x)) + residual
+        x = self.feed_forward_norm(x) if not self.normalize_before else x
+
+        # concatenate the cache
+        if cache is not None:
+            x = torch.cat([cache, x], dim=1)
+
+        return x, pos_emb
