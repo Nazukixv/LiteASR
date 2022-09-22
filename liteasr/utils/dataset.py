bde3307da193d060a023befc63750f92eedfbab2
Juncheng Cao <soukunsei@gmail.com>
2022-09-22 16:54:44 +0800

    :sparkles: Implement `RawAudioFileDataset`

--- liteasr/utils/dataset.py
+++ liteasr/utils/dataset.py
@@ -13,6 +13,7 @@ from liteasr.dataclass.sheet import AudioSheet
 from liteasr.dataclass.sheet import TextSheet
 from liteasr.utils.batchify import FrameBatch
 from liteasr.utils.batchify import SeqBatch
+from liteasr.utils.batchify import Wav2VecBatch
 from liteasr.utils.transform import PostProcess
 
 logger = logging.getLogger(__name__)
@@ -84,6 +85,55 @@ class AudioFileDataset(Dataset):
         return self.split == "train"
 
     def __getitem__(self, index):
+        """overload [] operator"""
+        if self.batchify_policy is None:
+            return self.data[index]
+        else:
+            return [self.data[idx] for idx in self.batchify_policy[index]]
+
+    def __len__(self):
+        """overload len() method"""
+        if self.batchify_policy is None:
+            return len(self.data)
+        else:
+            return len(self.batchify_policy)
+
+
+class RawAudioFileDataset(Dataset):
+
+    def __init__(self, data_cfg: str) -> None:
+        super().__init__()
+        self.data = []
+        self.batchify_policy = None
+
+        _as = AudioSheet(data_cfg)
+        for info in _as:
+            self.data.append(Audio(*info))
+
+            if len(self.data) % 10000 == 0:
+                logger.info("number of loaded data: {}".format(len(self.data)))
+        if len(self.data) % 10000 != 0:
+            logger.info("number of loaded data: {}".format(len(self.data)))
+
+    def batchify(self, dataset_cfg: DatasetConfig):
+        self.batchify_policy = Wav2VecBatch(dataset_cfg)
+        indices, _ = zip(
+            *
+            sorted(enumerate(self.data), key=lambda d: d[1].xlen, reverse=True)
+        )
+        self.batchify_policy.batchify(indices, self.data)
+
+    def set_postprocess(self, postprocess_cfg: PostProcessConfig):
+        pass
+
+    def collator(self, samples: List[List[Audio]]):
+        xs = []
+        min_batch_frame = min(samples[0][-1].xlen, 250000)
+        for sample in samples[0]:
+            xs.append(sample.x[:min_batch_frame])
+        return pad_sequence(xs, batch_first=True), None, None, None
+
+    def __getitem__(self, index: int):
         """overload [] operator"""
         if self.batchify_policy is None:
             return self.data[index]
